d99ec799efac720172b757fd1970d7cb3f2f0201
datttrian <datttrian@gmail.com>
2023-10-10 04:27:29 +0000

    Model binding ASP.NET Razor Pages

--- src/MoviesApp/Pages/AddMovie.cshtml.cs
+++ src/MoviesApp/Pages/AddMovie.cshtml.cs
@@ -1,28 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesApp.Data.Models;
 
 namespace MyApp.Namespace
 {
     public class AddMovieModel : PageModel
     {
+        // [BindProperty]
+        // public string Title { get; set; }
+        // [BindProperty]
+        // public int Rate { get; set; }
+        // [BindProperty]
+        // public string Description { get; set; }
         [BindProperty]
-        public string Title { get; set; }
-        [BindProperty]
-        public int Rate { get; set; }
-        [BindProperty]
-        public string Description { get; set; }
+        public Movie Movie {get;set;}
         // public void OnGetMyOnClick()
         // {
         //     string stopHere = "";
         // }
         public void OnGet()
         {
-            Title = "Welcome";
+            // Title = "Welcome";
         }
 
         public IActionResult OnPost()
         {
-            string value = $"{Title} - {Rate} - {Description}";
+            // string value = $"{Title} - {Rate} - {Description}";
+            string value = $"{Movie.Title} - {Movie.Rate} - {Movie.Description}";
             return Page();
             // return Redirect("Movies");
         }
