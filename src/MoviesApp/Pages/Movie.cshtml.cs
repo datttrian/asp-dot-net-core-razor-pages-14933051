48ccdbb5b5bfcbef6eb9c9a414a08d979e311a25
datttrian <datttrian@gmail.com>
2023-10-11 02:14:11 +0000

    Getting data by ID from a database

--- src/MoviesApp/Pages/Movie.cshtml.cs
+++ src/MoviesApp/Pages/Movie.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesApp.Data;
+using MoviesApp.Data.Models;
 
 namespace MyApp.Namespace
 {
     public class MovieModel : PageModel
     {
-        public void OnGet()
+        // public int Id {get;set;}
+        public Movie? Movie {get;set;}
+
+        private ApplicationDbContext _context;
+        public MovieModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public void OnGet(int id)
+        {
+            Movie = _context.Movies.FirstOrDefault(n => n.Id == id);
         }
     }
 }
